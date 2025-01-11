4a4af7f247ea0ec67f1dba8a7dbf3743c23b2d78
Rico van de Pol <ikbenpuntrico@gmail.com>
2025-01-11 15:08:28 +0100

    setup .gitignore

---
+++ Py21/main.py
@@ -0,0 +1,39 @@
+cards = {
+    1: {"num": "2",
+        "value": 2},
+    2: {"num": "3",
+        "value": 3},
+    3: {"num": "4",
+        "value": 4},
+    4: {"num": "5",
+        "value": 5},
+    5: {"num": "6",
+        "value": 6},
+    6: {"num": "7",
+        "value": 7},
+    7: {"num": "8",
+        "value": 8},
+    8: {"num": "9",
+        "value": 9},
+    9: {"num": "10",
+        "value": 10},
+    10: {"num": "J",
+         "value": 10},
+    11: {"num": "Q",
+         "value": 10},
+    12: {"num": "K",
+         "value": 10},
+    13: {"num": "A",
+         "value": 11}
+}
+
+suits = {
+    1: {"name": "Hearts",
+        "symbol": "H"},
+    2: {"name": "Diamonds",
+        "symbol": "D"},
+    3: {"name": "Clubs",
+        "symbol": "C"},
+    4: {"name": "Spades",
+        "symbol": "S"}
+}
